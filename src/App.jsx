48e43950055c6b68c372010d4683a777beb8bd5f
Francesca Lugay <chescalugay@gmail.com>
2025-10-14 04:45:23 +0800

    fixed the word: component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppointmentProvider } from "./context/AppointmentContext";
 import './index.css';
 import './App.css';
-import NavBar from "./components/NavBar";
+import NavBar from "./Components/NavBar";
 import Home from "./Routes/home";
 import Login from "./Routes/login";
 import Register from "./Routes/register";
