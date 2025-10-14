711bd207323c5226fcde457604579f797a6fe4ae
Francesca Lugay <chescalugay@gmail.com>
2025-10-14 17:47:41 +0800

    updated the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // <-- this line is important
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
