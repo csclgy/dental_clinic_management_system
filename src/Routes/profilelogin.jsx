1646c73d9661366c4a90e374e0c8f05b0a182bc8
Francesca Lugay <chescalugay@gmail.com>
2025-10-14 18:05:12 +0800

    fixed the loading

--- src/Routes/profilelogin.jsx
+++ src/Routes/profilelogin.jsx
@@ -115,7 +115,7 @@ const ProfileLogin = () => {
   <div className="flex justify-center items-center h-screen">
     <svg
       aria-hidden="true"
-      className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      className="w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
